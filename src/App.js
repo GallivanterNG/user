cf10080f173ad52ab162d08c6d6be0d98a5158dc
Amen Olabode <olabodeamen4@gmail.com>
2023-09-05 00:01:51 +0100

    install tailwind

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      User
+    <div className='flex h-screen w-full items-center justify-center font-semibold text-[88px]'>
+    User
     </div>
   );
 }
